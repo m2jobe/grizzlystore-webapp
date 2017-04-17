bc8c9b4cfedde7ebf14b9b6c414491258445e577
Muhammed Jobe <thejobemuhammed@gmail.com>
2017-04-16 21:19:48 -0400

    Update admin.php

--- admin.php
+++ admin.php
@@ -5,21 +5,21 @@
 <div class="container">
 <br/><br/>
 
-							<div>
-								<label>Title</label>
-								<input id="title" type="text" name="text" />				
-								<label>Price</label>
-								<input id="price" type="text" name="text" />
-								<label>Description</label>
-								<textarea id="description" /> </textarea>
-								<label>Image</label>
-								<input id="imageURL" type="text" name="text" />
-								
-									<button onclick="upload()" class="theme-button">
-										<i class="fa fa-upload"></i>Upload Now
-									</button>
-								 
-							</div>
+<div>
+<label>Title</label>
+<input id="title" type="text" name="text" />				
+<label>Price</label>
+<input id="price" type="text" name="text" />
+<label>Description</label>
+<textarea id="description" /> </textarea>
+<label>Image</label>
+<input id="imageURL" type="text" name="text" />
+
+<button onclick="upload()" class="theme-button">
+<i class="fa fa-upload"></i>Upload Now
+</button>
+
+</div>
 						
 </div>
 
